3bb407256f681fe860b9c39d1a9f7e4ce8a9306c
AtreyiKiit <83879678+AtreyiKiit@users.noreply.github.com>
2022-06-30 21:47:17 +0530

    Create CQRS_Pattern_practice.cs

---
+++ CQRS_Pattern_practice.cs
@@ -0,0 +1,95 @@
+// Query interface
+namespace ReadModel
+{
+  public interface ProductsDao
+  {
+    ProductDisplay FindById(int productId);
+    ICollection<ProductDisplay> FindByName(string name);
+    ICollection<ProductInventory> FindOutOfStockProducts();
+    ICollection<ProductDisplay> FindRelatedProducts(int productId);
+  }
+
+  public class ProductDisplay
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal UnitPrice { get; set; }
+    public bool IsOutOfStock { get; set; }
+    public double UserRating { get; set; }
+  }
+
+  public class ProductInventory
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int CurrentStock { get; set; }
+  }
+}
+
+
+
+//rating 
+public interface ICommand
+{
+  Guid Id { get; }
+}
+
+public class RateProduct : ICommand
+{
+  public RateProduct()
+  {
+    this.Id = Guid.NewGuid();
+  }
+  public Guid Id { get; set; }
+  public int ProductId { get; set; }
+  public int Rating { get; set; }
+  public int UserId {get; set; }
+}
+
+
+//handle commands
+public class ProductsCommandHandler :
+    ICommandHandler<AddNewProduct>,
+    ICommandHandler<RateProduct>,
+    ICommandHandler<AddToInventory>,
+    ICommandHandler<ConfirmItemShipped>,
+    ICommandHandler<UpdateStockFromInventoryRecount>
+{
+  private readonly IRepository<Product> repository;
+
+  public ProductsCommandHandler (IRepository<Product> repository)
+  {
+    this.repository = repository;
+  }
+
+  void Handle (AddNewProduct command)
+  {
+    ...
+  }
+
+  void Handle (RateProduct command)
+  {
+    var product = repository.Find(command.ProductId);
+    if (product != null)
+    {
+      product.RateProduct(command.UserId, command.Rating);
+      repository.Save(product);
+    }
+  }
+
+  void Handle (AddToInventory command)
+  {
+    ...
+  }
+
+  void Handle (ConfirmItemsShipped command)
+  {
+    ...
+  }
+
+  void Handle (UpdateStockFromInventoryRecount command)
+  {
+    ...
+  }
+}
